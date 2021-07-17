78816efac663d98b158ec657a9848680f77fa44d
Grismat <smirnalexandr@rambler.ru>
2021-07-17 13:29:08 +0300

    added getColumnIndicesRef method and hash optimized

--- src/model/Vertical.h
+++ src/model/Vertical.h
@@ -52,6 +52,7 @@ public:
     bool operator>(Vertical const& rhs) const { return !(*this < rhs && *this == rhs); }
 
     boost::dynamic_bitset<> getColumnIndices() const { return columnIndices; }
+    boost::dynamic_bitset<> const& getColumnIndicesRef() const { return columnIndices; }
     RelationalSchema const* getSchema() const { return schema; }
 
     bool contains(Vertical const& that) const;
