04d70d29a466ac016bdbab6ff62ea1da956b28f8
eduardG <almart_mail@mail.ru>
2021-11-15 17:34:48 +0300

    Fixed main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -101,7 +101,7 @@ int main(int argc, char const *argv[]) {
         algorithmInstance = std::make_unique<DFD>(path, separator, hasHeader);
     } else if (alg == "fastfds") {
         algorithmInstance = std::make_unique<FastFDs>(path, separator, hasHeader, maxLhs, parallelism);
-    } else if (alg == "depminer"){
+    } else if (alg == "depminer") {
         algorithmInstance = std::make_unique<Depminer>(path, separator, hasHeader);
     }
     try {
