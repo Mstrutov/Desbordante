Desbordante/desbordante-core

Added CMAXSet generator

src/main.cpp
@@ -15,15 +15,15 @@
 #include "algorithms/DFD/DFD.h"
 #include "algorithms/Fd_mine.h"
 #include "algorithms/FastFDs.h"
+#include "algorithms/Depminer.h"
 
 namespace po = boost::program_options;
 
 INITIALIZE_EASYLOGGINGPP
 
 bool checkOptions(std::string const& alg, double error) {
-    if (alg != "pyro" && alg != "tane" && alg != "fastfds" && alg != "fdmine" && alg != "dfd") {
+    if (alg != "pyro" && alg != "tane" && alg != "fastfds" && alg != "fdmine" && alg != "dfd" && alg != "depminer") {
         std::cout << "ERROR: no matching algorithm. Available algorithms are:\n\tpyro\n\ttane.\n" << std::endl;
-        return false;
     }
     if (error > 1 || error < 0) {
         std::cout << "ERROR: error should be between 0 and 1.\n" << std::endl;
@@ -98,6 +98,8 @@ int main(int argc, char const *argv[]) {
         algorithmInstance = std::make_unique<Fd_mine>(path);
     } else if (alg == "fastfds") {
         algorithmInstance = std::make_unique<FastFDs>(path, separator, hasHeader, maxLhs, parallelism);
+    } else if (alg == "depminer"){
+        algorithmInstance = std::make_unique<Depminer>(path, separator, hasHeader);
     }
     try {
         unsigned long long elapsedTime = algorithmInstance->execute();

Mstrutov/Desbordante

src/main.cpp
@@ -15,15 +15,15 @@
 #include "algorithms/DFD/DFD.h"
 #include "algorithms/Fd_mine.h"
 #include "algorithms/FastFDs.h"
+#include "algorithms/Depminer.h"
 
 namespace po = boost::program_options;
 
 INITIALIZE_EASYLOGGINGPP
 
 bool checkOptions(std::string const& alg, double error) {
-    if (alg != "pyro" && alg != "tane" && alg != "dfd" && alg != "fastfds" && alg != "fdmine") {
+    if (alg != "pyro" && alg != "tane" && alg != "dfd" && alg != "fastfds" && alg != "fdmine" && alg != "depminer") {
         std::cout << "ERROR: no matching algorithm. Available algorithms are:\n\tpyro\n\ttane.\n" << std::endl;
-        return false;
     }
     if (error > 1 || error < 0) {
         std::cout << "ERROR: error should be between 0 and 1.\n" << std::endl;
@@ -100,6 +100,8 @@ int main(int argc, char const *argv[]) {
         algorithmInstance = std::make_unique<DFD>(path, separator, hasHeader);
     } else if (alg == "fastfds") {
         algorithmInstance = std::make_unique<FastFDs>(path, separator, hasHeader, maxLhs, parallelism);
+    } else if (alg == "depminer"){
+        algorithmInstance = std::make_unique<Depminer>(path, separator, hasHeader);
     }
     try {
         unsigned long long elapsedTime = algorithmInstance->execute();